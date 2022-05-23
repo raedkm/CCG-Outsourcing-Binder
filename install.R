caa7b326208946fa03bcf791faa2290f7544f87b
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2022-05-23 18:14:20 +0100

    Update install.R

--- install.R
+++ install.R
@@ -3,9 +3,8 @@ install.packages(c("devtools","knitr", "dplyr", "dotwhisker", "tidyverse",
 "plm", "extrafont","texreg", "CBPS", "sp", "maptools", "sf", "gt", "gtsummary","modelsummary", 
 "regclass", "stargazer", "pbkrtest", "sjPlot", "lme4", "clubSandwich", "lmerTest", "sf", "sp", "spdep",
 "rgdal", "rgeos", "tmap", "tmaptools",  "spgwr", "grid", "gridExtra","curl", "kableExtra",  "MuMIn", "parallel",
-"tinytex", "zen4R", "cowplot", "ggforce", "ggthemes", "inborutils"))
+"tinytex", "zen4R", "cowplot", "ggforce", "ggthemes"))
 
 library(devtools)
 
-install_github("inbo/inborutils")
 install_github("BenGoodair/CCG-Outsourcing", ref = "master")
