cccd9c86d9af34dcbf2dfe7728d15fe7c4fedf1c
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2022-05-23 17:38:38 +0100

    Update install.R

--- install.R
+++ install.R
@@ -5,6 +5,7 @@ install.packages(c("devtools","knitr", "dplyr", "dotwhisker", "tidyverse",
 "rgdal", "rgeos", "tmap", "tmaptools",  "spgwr", "grid", "gridExtra","curl", "kableExtra",  "MuMIn", "parallel",
 "tinytex", "zen4R", "cowplot", "ggforce", "ggthemes", "inborutils"))
 
-install.packages("inborutils", repos = c(inbo = "https://inbo.r-universe.dev"))
+library(devtools)
 
+install_github("inbo/inborutils")
 install_github("BenGoodair/CCG-Outsourcing", ref = "master")
