7ffcd88a9dc64aac498867be78b1743f2e06ca92
Ben Goodair <63054419+BenGoodair@users.noreply.github.com>
2022-05-23 17:12:23 +0100

    Update install.R

--- install.R
+++ install.R
@@ -2,6 +2,7 @@ install.packages(c("devtools","knitr", "dplyr", "dotwhisker", "tidyverse",
 "pastecs", "stringr", "rebus", "cobalt", "pracma", "zoo", "future.apply", "runner",
 "plm", "extrafont","texreg", "CBPS", "sp", "maptools", "sf", "gt", "gtsummary","modelsummary", 
 "regclass", "stargazer", "pbkrtest", "sjPlot", "lme4", "clubSandwich", "lmerTest", "sf", "sp", "spdep",
-"rgdal", "rgeos", "tmap", "tmaptools",  "spgwr", "grid", "gridExtra","curl", "kableExtra",  "MuMIn", "parallel", "tinytex", "zen4R", "cowplot", "ggforce", "ggthemes"))
+"rgdal", "rgeos", "tmap", "tmaptools",  "spgwr", "grid", "gridExtra","curl", "kableExtra",  "MuMIn", "parallel",
+"tinytex", "zen4R", "cowplot", "ggforce", "ggthemes", "inborutils"))
 
 
